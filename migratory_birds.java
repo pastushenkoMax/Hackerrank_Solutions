98ba833487f8caedf12c5bbe005b16378e644ab5
Maksym Pastushenko <76412885+pastushenkoMax@users.noreply.github.com>
2022-12-14 22:50:19 -0700

    Update migratory_birds.java

--- migratory_birds.java
+++ migratory_birds.java
@@ -4,48 +4,29 @@ import java.util.stream.IntStream;
 
 public class migratory_birds {
     public static int migratoryBirds(List<Integer> arr) {
-            // arr.forEach(i -> System.out.println(arr.get(i)));
             int type = 0;
-        int id_1 = 0;
-        int id_2 = 0;
-        int id_3 = 0;
-        int id_4 = 0;
-        int id_5 = 0;
+        int[] counter_arr = {0,0,0,0,0};
         for(int i = 0; i < arr.size(); i++){
             if(arr.get(i) == 1){
-                id_1++;
+                counter_arr[0]++;
             } else if(arr.get(i) == 2){
-                id_2++;
+                counter_arr[1]++;
             } else if(arr.get(i) == 3){
-                id_3++;
+                counter_arr[2]++;
             } else if(arr.get(i) == 4){
-                id_4++;
+                counter_arr[3]++;
             } else{
-                id_5++;
+                counter_arr[4]++;
             }
         }
-        
-        if(id_5 > id_4){
-            type = 5;
-        } else if(id_4 > id_3){
-            type = 4;
-        } else if(id_3 > id_2){
-            type = 3;
-        } else if(id_2 > id_1){
-            type = 2;
-        } else {
-            type = 1;
+        int max = counter_arr[0];
+        for (int i = 1; i < counter_arr.length; i++){
+            if (counter_arr[i] > max){
+                max = counter_arr[i];
+                type = i+1;
+            }
         }
-    return type; 
-        // int count = 0;
-        // List<Integer> temp = new LinkedList<Integer>();
-        // IntStream.range(0, n)
-        // .forEach(i -> {if(temp.contains(ar.get(i))){
-        //     temp.remove(ar.get(i));
-        //     }else{temp.add(ar.get(i));
-        //     }});
-        // int count = (ar.size() - temp.size())/2;
-        // return count;
+        return type;
     }
     
     public static void main(String[] args){
